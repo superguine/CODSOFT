be914fa7930e9c7b95c3ddccd54da9a3d67917ea
Shawon Roy <shawonfedora@gmail.com>
2024-07-14 18:19:45 +0530

    Task 2 updated

--- GradeCalc.java
+++ GradeCalc.java
@@ -6,10 +6,31 @@ public class GradeCalc {
     {
     
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Please Enter the number of Subjects: ");
         int sub = sc.nextInt();
-        int subjects[]=new int[sub];
-        int marks[]=new int[sub];
         
+        String subjects[]=new String[sub];
+        int marks[]=new int[sub];
+        String subject;
+
+        System.out.println("\n Enter names of "+sub+" Subjects one by one \n");
+        sc.nextLine();
+
+        for (int i=0;i<sub;i++)
+        {
+            System.out.print("Enter subject  "+(i+1)+" :->  ");
+            subject = sc.nextLine();
+            subjects[i]= subject;
+        }
+
+        int mark=0;
+        System.out.println("\n Enter marks according to those Subjects one by one \n");
+        for (int j=0;j<sub;j++)
+        {
+            System.out.print("Enter the marks obtained in "+subjects[j]+" :->  ");
+            mark = sc.nextInt();
+            marks[j]= mark;
+        }
     }
 }
