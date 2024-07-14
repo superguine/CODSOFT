5e8c69e9d26b5a3c3ad32a78df88a764e009756d
Shawon Roy <shawonfedora@gmail.com>
2024-07-14 17:19:29 +0530

    Task 2

---
+++ GradeCalc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class GradeCalc {
+
+    
+    public static void main(String args[])
+    {
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please Enter the number of Subjects: ");
+        int sub = sc.nextInt();
+        int subjects[]=new int[sub];
+        int marks[]=new int[sub];
+        
+    }
+}
